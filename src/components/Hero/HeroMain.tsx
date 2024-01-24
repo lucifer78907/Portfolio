0b90e55f8dad2793427e3eb5a9f617e73852084e
Rudra Pratap Singh <chaudhary66667@gmail.com>
2024-01-24 22:57:57 +0530

    +HeroMain BreadCrumbs

--- src/components/Hero/HeroMain.tsx
+++ src/components/Hero/HeroMain.tsx
@@ -1,4 +1,6 @@
 import "./HeroMain.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const HeroMain = () => {
   return (
@@ -7,6 +9,20 @@ const HeroMain = () => {
         <span className="heading__primary--hindi">नमस्ते</span>I am Rudra
         <span className="heading__primary--sub">A developer</span>
       </h1>
+      <span className="hero__bottom-line">
+        <span className="hero__breadcrumb hero__breadcrumb--react">React</span>
+        {/* <span className="hero__breadcrumb hero__breadcrumb--node">Node.js</span>
+        <span className="hero__breadcrumb hero__breadcrumb--mongodb">
+          MongoDb
+        </span>
+        <span className="hero__breadcrumb hero__breadcrumb--design">
+          Design
+        </span>
+        <span className="hero__breadcrumb hero__breadcrumb--rest">
+          Rest API
+        </span> */}
+      </span>
+      {/* <span className="hero__circle"></span> */}
     </section>
   );
 };
