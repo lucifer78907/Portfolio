5fc58973fc2363f6ce402b3c8fa2abfeed488647
Rudra Pratap Singh <chaudhary66667@gmail.com>
2024-02-02 23:50:41 +0530

    +Added hero_about

--- src/components/Hero/HeroMain.tsx
+++ src/components/Hero/HeroMain.tsx
@@ -88,6 +88,22 @@ const HeroMain = () => {
     <section className="hero__main" ref={scope}>
       <h1 className="heading__primary">
         I am
+        <span className="hero__details">
+          <article className="hero__about">
+            <p className="hero__about--title">pronoun</p>
+            <p className="hero__about--subtitle">he/him/dev</p>
+          </article>
+          <div className="hero__line"></div>
+          <article className="hero__about">
+            <p className="hero__about--title">adjective</p>
+            <p className="hero__about--subtitle">innovative/result-driven</p>
+          </article>
+          <div className="hero__line"></div>
+          <article className="hero__about">
+            <p className="hero__about--title">adverb</p>
+            <p className="hero__about--subtitle">efficient/elegant</p>
+          </article>
+        </span>
         <span className="heading__primary--sub" id="main"></span>
       </h1>
       <div className="hero__background"></div>
